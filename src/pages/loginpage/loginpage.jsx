cd97ad7da7c36746e155afa6d24efcbf076ced87
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2022-04-17 21:20:59 +0700

    add signin

--- src/pages/loginpage/loginpage.jsx
+++ src/pages/loginpage/loginpage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./loginpage.scss";
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="loginpage-root">
+      <form className="section">
+        <h1>เข้าสู่ระบบ</h1>
+        <div className="field">
+          <p>อีเมล</p>
+          <input type="text" placeholder="อีเมล" />
+        </div>
+        <div className="field">
+          <p>รหัสผ่าน</p>
+          <input type="text" placeholder="รหัสผ่าน" />
+        </div>
+        <button>เข้าสู่ระบบ</button>
+        <Link to={"register"}>
+          <p>
+            ไม่มีบัญชีผู้ใช้ ?<span> ลงทะเบียน</span>
+          </p>
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
