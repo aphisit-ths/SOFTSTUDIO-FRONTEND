c7958a11fb1a8498a367b92be98395edf14f188b
rapeepatpe <54279530+rapeepatpe@users.noreply.github.com>
2022-04-18 17:46:19 +0700

    Overview Page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['src/**/**/*.jsx',
+  'src/**/*.jsx',
+  'src/**/*.ts',
+  'src/**/*.tsx',
+  'public/**/*.html',],
+  theme: {
+    extend: {
+      colors: {
+        'Custom-BG-Grey': '#F6F6F6',
+      },
+    },
+  },
+  plugins: [],
+}
